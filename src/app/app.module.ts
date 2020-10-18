58ce7677e0cca497037875d2cb0e499d31ca24ec
Shrey <shreysingla@cse.iitb.ac.in>
2020-10-18 13:08:40 +0530

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormComponent } from './form/form.component';
+import { ContactComponent } from './contact/contact.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FormComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
