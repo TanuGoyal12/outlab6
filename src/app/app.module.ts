10295e81d3ef3a26a40e76f3709a832fc047f35b
TanuGoyal <tanugoyal.iitb@gmail.com>
2020-10-18 13:44:41 +0530

    reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 import { FormComponent } from './form/form.component';
 import { ContactComponent } from './contact/contact.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HeaderComponent } from './header/header.component';
+// import { MatInputModule } from '@angular/material/input';
 
-import { HttpClientModule } from '@angular/common/http'
-import { ReactiveFormsModule } from '@angular/forms';
-
+// import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FormComponent,
-    ContactComponent
+    ContactComponent,
+    HeaderComponent,
+    // ProfileEditorComponent
   ],
   imports: [
     BrowserModule,
+      ReactiveFormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    // NoopAnimationsModule,
+     HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-
+// import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import {DataService} from '../data.service';
+import { HttpErrorResponse } from '@angular/common/http';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit{
+    form: FormGroup;
+  arr = { name: "", email: "" ,feedback: "",comment: ""};
 
-  constructor() { }
+  constructor( private dataservice: DataService, builder: FormBuilder) {
+    this.form = builder.group({
+      name: "",
+      email: "",
+      feedback: "",
+       comment: ""
+    });
 
-  ngOnInit(): void {
+    
+     this.dataservice.getConfig() 
+    .subscribe( Response => { 
+this.arr.email=Response.email;
+this.arr.comment=Response.comment;
+this.arr.feedback=Response.feedback;
+      this.arr.name = Response.name;
+    }); 
+    // this.ngOnInit();
+
   }
+   ngOnInit(): void { 
+
+ }
+
+
+submitFunc() {
+  this.form.value.comment= this.arr.comment;
+    this.dataservice.postConfig(this.form.value)
+                .subscribe(
+                    (data) => {
+                       console.log('Form submitted successfully');  
+                       console.log(JSON.stringify(data));
+                       document.getElementById("validation").innerHTML = "Form submitted successfully!";
+
+                    },
+                    (error: HttpErrorResponse) => {
+                        console.log(error);
+                         document.getElementById("validation").innerHTML = "An error occurred while submitting ):";
+                    }
+                );
+        }
+
+
+
+ 
+
+
+
 
 }
